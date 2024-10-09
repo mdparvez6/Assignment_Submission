fcb718dbcb3293b1687d007de5650d80c3036e8a
mdparvez6 <mdparvez1466@gmail.com>
2024-10-09 19:15:19 +0530

    added authentication

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -3,25 +3,26 @@ import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
 import jwt from "jsonwebtoken";
 
-export const verifyJWT = asyncHandler(async (req, _, next) => {
-  try {
-    const token = req.header("Authorization");
+export const verifyJWT = (role) =>
+  asyncHandler(async (req, _, next) => {
+    try {
+      const token = req.header("Authorization");
 
-    if (!token) {
-      throw new ApiError(401, "Unauthorized request");
-    }
+      if (!token) {
+        throw new ApiError(401, "Unauthorized request");
+      }
 
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    const user = await User.findById(decoded.id).select("-password");
-    if (!user) {
-      throw new ApiError(401, "Invalid Acess Token");
+      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decoded.id).select("-password");
+      if (!user) {
+        throw new ApiError(401, "Invalid Acess Token");
+      }
+      if (user.role !== role) {
+        throw new ApiError(403, "Unauthorized role");
+      }
+      req.user = user;
+      next();
+    } catch (error) {
+      throw new ApiError(401, error?.message || "Invalid access token");
     }
-    if (user.role !== role) {
-      throw new ApiError(403, "Unauthorized role");
-    }
-    req.user = user;
-    next();
-  } catch (error) {
-    throw new ApiError(401, error?.message || "Invalid access token");
-  }
-});
+  });
