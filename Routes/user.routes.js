7202dd5e7adae18e7ddbf8e27ab0481da69cb2a3
mdparvez6 <mdparvez1466@gmail.com>
2024-10-10 20:25:36 +0530

    added user routes

---
+++ Routes/user.routes.js
@@ -0,0 +1,20 @@
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  uploadAssignment,
+  getAllAdmins,
+} from "../Controllers/user.controllers.js";
+import { verifyJWT } from "../Middleware/auth.middleware.js";
+
+const router = express.Router();
+
+router.user("/register", registerUser);
+
+router.post("/login", loginUser);
+
+router.post("upload", verifyJWT("user"), uploadAssignment);
+
+router.get("/admins", getAllAdmins);
+
+export default router;
