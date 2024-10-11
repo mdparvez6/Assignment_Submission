514f0938aca2b5ce911b2c8371bca0d88a8e46b7
mdparvez6 <mdparvez1466@gmail.com>
2024-10-11 06:37:14 +0530

    added auth middleware

--- Middleware/auth.middleware.js
+++ Middleware/auth.middleware.js
@@ -12,7 +12,7 @@ export const verifyJWT = (role) =>
         throw new ApiError(401, "Unauthorized request");
       }
 
-      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
       const user = await User.findById(decoded.id).select("-password");
       if (!user) {
         throw new ApiError(401, "Invalid Acess Token");
